705b04d37dfd8a6ac92e728d6f3470e41508785c
dremerdima3 <dmitrijkonopla20@gmail.com>
2025-02-07 01:00:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-const tg = window.Telegram.WebApp;
-
-// Инициализация
-tg.expand(); // Развернуть приложение на весь экран
-tg.MainButton.show();
-tg.MainButton.setText("Закрыть");
-tg.MainButton.onClick(() => tg.close());
-
-// Получение данных пользователя
-const user = tg.initDataUnsafe.user;
-if (user) {
-    console.log("Привет, " + user.first_name + "!");
-}
-
-// Логика кликера
 let counter = 0;
-let energy = 300;
-const counterElement = document.getElementById('counter');
-const clickButton = document.getElementById('clickButton');
-
-clickButton.addEventListener('click', () => {
-    counter++;
-    counterElement.textContent = counter;
-});
-/*let counter = 0;
 let energy = 300;
 let maxEnergy = 300;
 let clickMultiplier = 1; // Множитель очков за клик
